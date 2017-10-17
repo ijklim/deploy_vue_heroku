8553a890b6ceaf809e4b775acb4fa88959b20c9b
Ivan Lim <aimprove@gmail.com>
2017-10-17 12:32:43 -0400

    Invoke connect-history-api-fallback

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+// var path = require('path');
+// var serveStatic = require('serve-static');
 
-app = express();
-app.use(serveStatic(__dirname));
+var app = express();
+var history = require('connect-history-api-fallback');
+
+// app.use(serveStatic(__dirname));
+app.use(history());
 
 var port = process.env.PORT || 5000;
 app.listen(port);
