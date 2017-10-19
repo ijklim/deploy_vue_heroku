49019a7311455f73775f184aa963bb1da5fbaf56
Ivan Lim <aimprove@gmail.com>
2017-10-19 11:26:26 -0400

    Transfer https redirect logic from .htaccess to server.js

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ var history = require('connect-history-api-fallback');
 
 var app = express();
 
+var https_redirect = function(req, res, next) {
+  if (process.env.NODE_ENV === "production") {
+    if (req.headers["x-forwarded-proto"] != "https") {
+      return res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      return next();
+    }
+  } else {
+    return next();
+  }
+};
+
+app.use(https_redirect);
 app.use(history());
 app.use(serveStatic(__dirname));
 
